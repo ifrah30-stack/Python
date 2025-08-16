af5e054723a6ab1652d5fb0ea8a040090e315ccf
ifrah30-stack <Ifrahanjum3004@gmail.com>
2025-08-16 19:51:31 +0530

    Create String Manipulation.py

---
+++ String Manipulation.py
@@ -0,0 +1,4 @@
+text = "Python Programming"
+print(text.lower())  # Output: "python programming"
+print(text.split())  # Output: ['Python', 'Programming']
+print(text.replace("Python", "Java"))  # Output: "Java Programming"
