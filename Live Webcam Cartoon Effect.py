2419861e7ac029c46b98a791cfc2716923300ade
ifrah30-stack <Ifrahanjum3004@gmail.com>
2025-08-27 19:46:12 +0530

    Create Live Webcam Cartoon Effect.py

---
+++ Live Webcam Cartoon Effect.py
@@ -0,0 +1,17 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 7)
+    edges = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                  cv2.THRESH_BINARY, 9, 9)
+    color = cv2.bilateralFilter(frame, 9, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+    cv2.imshow("Cartoon", cartoon)
+    if cv2.waitKey(1) == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
