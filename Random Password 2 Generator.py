9f80b4835d96857f7147299d1dee261cc73bd6b0
ifrah30-stack <Ifrahanjum3004@gmail.com>
2025-09-05 19:18:57 +0530

    Create Random Password 2 Generator.py

---
+++ Random Password 2 Generator.py
@@ -0,0 +1,7 @@
+import random, string
+
+def generate_password(length=8):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(chars) for _ in range(length))
+
+print(generate_password(12))
