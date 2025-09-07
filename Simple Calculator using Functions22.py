db173da6ebeabaae831c7aeba10572def964a7eb
ifrah30-stack <Ifrahanjum3004@gmail.com>
2025-09-07 20:52:40 +0530

    Create Simple Calculator using Functions22.py

---
+++ Simple Calculator using Functions22.py
@@ -0,0 +1,9 @@
+def add(a,b): return a+b
+def sub(a,b): return a-b
+def mul(a,b): return a*b
+def div(a,b): return a/b if b!=0 else "Undefined"
+
+print(add(5,3))
+print(sub(10,4))
+print(mul(6,7))
+print(div(8,2))
